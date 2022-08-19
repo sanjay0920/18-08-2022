9ddf4cc88c03e7be3d1b99cbcaa7c9af61980bc5
sanjay0920 <111352551+sanjay0920@users.noreply.github.com>
2022-08-19 11:05:58 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,11 +1,24 @@
 ﻿using System;
-public class age
+public class average
 {
     public static void Main()
     {
-        int age;
-        Console.Write("Enter your age ");
-        age = Convert.ToInt32(Console.ReadLine());
-        Console.Write("You look younger than {0} ", age);
+        double number1, number2, number3, number4;
+
+        Console.Write("Enter the First number: ");
+        number1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the Second number: ");
+        number2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the third number: ");
+        number3 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the fourth number: ");
+        number4 = Convert.ToDouble(Console.ReadLine());
+
+        double result = (number1 + number2 + number3 + number4) / 4;
+        Console.WriteLine("The average of {0}, {1}, {2}, {3} is: {4} ",
+     number1, number2, number3, number4, result);
     }
 }
